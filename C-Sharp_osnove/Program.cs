5921d7b974831d77044f0972615bdcd2c74c9376
Matija Luketin <matijaluketin@gmail.com>
2020-11-22 19:52:22 +0100

    Final version

--- C-Sharp_osnove/Program.cs
+++ C-Sharp_osnove/Program.cs
@@ -13,11 +13,17 @@ namespace C_Sharp_osnove
         {
             Console.WriteLine("Hello World!");
 
-            //7
+            //8
+            //int, long, short, float, double, decimal, char, string
             int firstInt = 3;
             int secondInt = 5;
+            bool trueOrFalse = true;
+            char letterOfTheAlphabet = 'a';
+            string someString = "hello";
+            //9
+            var thirdInt = 8;
 
-            //8
+            //10
             Console.WriteLine(firstInt + secondInt);
             Console.WriteLine(firstInt - secondInt);
             Console.WriteLine(secondInt / firstInt);
@@ -28,11 +34,11 @@ namespace C_Sharp_osnove
             double decimalNumber = 123.45;
             Console.WriteLine(decimalNumber / secondInt);
 
-            //9
+            //11
             //double enteredNumber = int.Parse(Console.ReadLine());
             double enteredNumber = double.Parse(Console.ReadLine());
 
-            //10
+            //12
             if(enteredNumber > decimalNumber)
                 Console.WriteLine("uneseni je veci");
             else if(firstInt == decimalNumber)
@@ -40,60 +46,106 @@ namespace C_Sharp_osnove
             else
                 Console.WriteLine("uneseni je manji");
 
-            //11
+            var enteredInteger = int.Parse(Console.ReadLine());
+
+            switch (enteredInteger)
+            {
+                case 1:
+                    Console.WriteLine("Wow unio si broj jedan!");
+                    break;
+                case 2:
+                    Console.WriteLine("Wow unio si broj dva!");
+                    break;
+                case 3:
+                    Console.WriteLine("Wow unio si broj tri!");
+                    break;
+                case 4:
+                    Console.WriteLine("Wow unio si broj četiri!");
+                    break;
+                default:
+                    break;
+            }
+
+            //13
             var numberOfHours = int.Parse(Console.ReadLine());
             var moneyPaidOut = double.Parse(Console.ReadLine());
+            var decision = 0;
 
             var payPerHour = moneyPaidOut / numberOfHours;
             Console.WriteLine(payPerHour);
 
-            if(payPerHour >= 50)
-                Console.WriteLine("Moze");
-            else
-                Console.WriteLine("Ne moze");
+            if (payPerHour < 50)
+                decision = 1;
+            else if (payPerHour >= 100)
+                decision = 2;
 
-            //12
-            //var variable;
+            //14
+            var enumDecision = Decision.Considering;
 
-            //13
+            var payPerHourAgain = moneyPaidOut / numberOfHours;
+            Console.WriteLine(payPerHour);
+
+            if (payPerHourAgain < 50)
+                enumDecision = Decision.Refused;
+            else if (payPerHourAgain >= 100)
+                enumDecision = Decision.Accepted;
+
+            Console.WriteLine(enumDecision);
+
+            //15
             var myList = new List<int>() { 4, 8, 12 };
 
             myList.Add(16);
             myList.Remove(8);
             Console.WriteLine(myList.IndexOf(4));
 
-            //14
+            //16
+            var i = 0;
+            while (i < myList.Count)
+            {
+                Console.WriteLine(myList[i]);
+                i++;
+            }
+
+            for (var j = 0; j < myList.Count; j++)
+            {
+                myList[j] += 4;
+                Console.WriteLine(myList[j]);
+            }
+
+            var elementCounter = 0;
             foreach (var item in myList)
             {
-                //item += 4;
+                elementCounter++;
                 Console.WriteLine(item);
             }
+            Console.WriteLine(elementCounter);
 
-            for (int i = 0; i < myList.Count; i++)
-            {
-                myList[i] += 4;
-                Console.WriteLine(myList[i]);
-            }
-
-            for (int i = 0; i < myList.Count; i++)
-            {
-                myList[i] += 4;
-                Console.WriteLine(myList[i]);
-            }
-
-            //15
+            //17
             var myString = "    string";
             myString += " je imutabilan     ";
             Console.WriteLine(myString);
             Console.WriteLine($"{myString} interpoliramo ovako");
+            Console.WriteLine("ovo je kompozitno formatiranje: {0}", myString );
 
             Console.WriteLine(myString.Trim().ToUpper());
 
-            //16
+            //18
             var verse = "stani lave stani";
             Console.WriteLine(ReverseString(verse));
 
-            //17
+            //20
+            var myTuple = new Tuple<string, string>("srebrni", "Opel");
+
+            Console.WriteLine(myTuple.Item1);
+
+            var myNewTuple = ( "hello", true );
+            Console.WriteLine(myNewTuple.Item1);
+
+            var myNamedTuple = (Color: "srebrni", Brand: "Opel");
+            Console.WriteLine(myNamedTuple.Color);
+
+            //21
             var myDictionary = new Dictionary<int, string>()
             {
                 {1312, "hej" },
@@ -117,15 +169,7 @@ namespace C_Sharp_osnove
                 Console.WriteLine(pair.Value);
             }
 
-            //18
-            var myTuple = new Tuple<string, string>("srebrni", "Opel");
-
-            Console.WriteLine(myTuple.Item1);
-
-            var myNamedTuple = (Color: "srebrni", Brand: "Opel");
-            Console.WriteLine(myNamedTuple.Color);
-
-            //19
+            //22
             var officeObjects = new Dictionary<string, int>()
             {
                 {"Printer", 10 },
@@ -133,9 +177,25 @@ namespace C_Sharp_osnove
                 {"Projektor", 5 }
             };
 
-            var lightestObject = FindLightestObject(officeObjects);
+            var officeObject = FindLightestObject(officeObjects);
+            Console.WriteLine(officeObject.Item1 + ' ' + officeObject.Item2);
 
-            Console.WriteLine(lightestObject);
+            var lightestObject = FindLightestObjectNamed(officeObjects);
+            Console.WriteLine(lightestObject.name + ' ' + lightestObject.weight);
+
+            (var name, var weight) = FindLightestObjectNamed(officeObjects);
+            Console.WriteLine(name + ' ' + weight);
+
+            (var destructuredName, var destructuredWeight) = FindLightestObject(officeObjects);
+            Console.WriteLine(destructuredName + ' ' + destructuredWeight);
+        }
+
+        //14
+        enum Decision
+        {
+            Considering,
+            Accepted,
+            Refused
         }
 
         //16
@@ -169,7 +229,7 @@ namespace C_Sharp_osnove
 
             foreach (var officeObject in objectsInOffice)
             {
-                if(officeObject.Value < lightestObject.Item2 || lightestObject.Item2 == 0)
+                if (officeObject.Value < lightestObject.Item2 || lightestObject.Item2 == 0)
                 {
                     lightestObject = new Tuple<string, int>(officeObject.Key, officeObject.Value);
                 }
@@ -178,5 +238,19 @@ namespace C_Sharp_osnove
             return lightestObject;
         }
 
+        static (string name, int weight) FindLightestObjectNamed(Dictionary<string, int> objectsInOffice)
+        {
+            var lightestObject = ("None", 0);
+
+            foreach (var officeObject in objectsInOffice)
+            {
+                if(officeObject.Value < lightestObject.Item2 || lightestObject.Item2 == 0)
+                {
+                    lightestObject = (officeObject.Key, officeObject.Value);
+                }
+            }
+
+            return lightestObject;
+        }
     }
 }
